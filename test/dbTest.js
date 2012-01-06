0fcc0c6b9617d07cb10926cf182f0198fb8fbde8
Markus Pielmeier <markus.pielmeier@googlemail.com>
2012-01-06 13:10:37 +0100

    added test case which validates transactional behaviour

--- test/dbTest.js
+++ test/dbTest.js
@@ -119,6 +119,10 @@ function getEmptyDB(name, success){
 	    var ens = CallEnsurance();
 	    var key = '___myWeirdName:/\\?&=___';
 
+	    var testSuccess = ens.ensureCall(test + ' getter', function(entry){
+		    assert.equal('hello world!', entry.value);
+		});
+
 	    (function(){
 		var t = db.newTransaction();
 	
@@ -130,14 +134,55 @@ function getEmptyDB(name, success){
 	    (function(){
 		var t = db.newTransaction(true);
 
-		t.get(key, ens.ensureCall(test + ' getter', function(entry){
-			    assert.equal('hello world!', entry.value);
-			}),
-		    assert.fail);
+		t.get(key, testSuccess, assert.fail);
 	    })();
 
 	    ens.waitForCalls(10 * 1000);
 	});
-})();
+}());
 
-// TODO unit test which validates transactional behavior
+(function(){
+    var test = 'testTransactionalCommitAndRollbackBehaviour';
+    console.log('Running ' + test);
+    getEmptyDB(test, function(db){
+	    var ens = CallEnsurance();
+	    var key = 'key';
+	    var value = 'value';
+
+	    var testSuccess = ens.ensureCall(test + ' testSuccess', function(){});
+
+	    (function(){
+		var t = db.newTransaction();
+
+		t.set(key, value);
+
+		t.commit();
+	    }());
+
+	    function rollbackTest(success){
+		var t = db.newTransaction();
+
+		t.set(key, 'something awful');
+
+		t.rollback();
+
+		success();
+	    }
+
+	    function assertValue(success){
+		db.newTransaction(true).get(key, function(data){
+			assert.equal(value, data);
+			
+			success();
+		    }, assert.fail);
+	    }
+
+	    assertValue(function(){
+		    rollbackTest(function(){
+			    assertValue(testSuccess);
+			});
+		});
+
+	    ens.waitForCalls(10 * 1000);
+	});
+}());
